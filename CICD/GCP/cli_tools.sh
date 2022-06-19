496c03cc82a434a09577da92bc21fd0d9cb3449b
jmake <zvl_2006@hotmail.com>
2022-06-19 14:46:03 +0200

    Update cli_tools.sh

--- CICD/GCP/cli_tools.sh
+++ CICD/GCP/cli_tools.sh
@@ -1,13 +1,30 @@
 
-echo "[GCP] "
 
-gcloud version
+GCP_INIT() 
+{
+  echo "[GCP_INIT]"
+  ${GCLOUD} --version 
+  
+  ${GCLOUD} iam service-accounts list 
+  ${GCLOUD} auth list
+  
+  ${GCLOUD} projects list
+}
 
 
 
-echo "[GCP] "
+
+#
+GCLOUD=gcloud
+GCP_INIT
+
 
 
 # REFERENCES :
+#
+# https://medium.com/@jmake/platform-as-a-service-paas-is-a-cloud-computing-model-where-a-third-party-provider-delivers-52aacda65a90
+#
+# https://github.com/jmake/ipynbs/blob/main/googleAppEngine.ipynb
+#
 # https://cloud.google.com/blog/topics/developers-practitioners/deploying-serverless-platforms-github-actions
 #
